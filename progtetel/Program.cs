fff921e38f061e80faf5013f4af3ba37c3523229
Háberl Kristóf József <haberl.kristof@ckik.loc>
2024-12-04 11:12:24 +0100

    ittvan more

--- progtetel/Program.cs
+++ progtetel/Program.cs
@@ -9,6 +9,21 @@ namespace progtetel
             int[] x = { 7, 2, -5, 12, 5, 1 };
 
             //Párosak kiválogatása - 1
+            int[] paros_szamok = new int[100];
+            int db = 0;
+            int i;
+            for (i = 0; 1 <= x.Length; i++)
+            {
+                if (x[i] % 2 == 0)
+                {
+                    paros_szamok[db] = x[i];
+                    db++;
+                }
+            }
+            for (i = 0; i < db; i++)
+            {
+                Console.WriteLine(paros_szamok[i] + " ");
+            }
 
             // Maximum kiválasztás - 2
         }
