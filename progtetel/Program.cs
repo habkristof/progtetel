69864ca181840ed60611eace4738b5f0e3299397
harceszko <harczi.zsombor@ckik.hu>
2024-12-04 11:16:44 +0100

    carryzlek fifamatyi

--- progtetel/Program.cs
+++ progtetel/Program.cs
@@ -11,6 +11,21 @@ namespace progtetel
             //Párosak kiválogatása - 1
 
             // Maximum kiválasztás - 2
+            int i;
+            int n = x.Length;
+
+            int maxi = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (x[i] > x[maxi])
+                {
+                    maxi = i;
+                }
+            }
+            Console.WriteLine($"{maxi + 1}. , {x[maxi]}");
+
+
+            Console.ReadKey();
         }
     }
 }
